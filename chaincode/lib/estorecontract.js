38ca76f155c58629eb79445c1887e1d5188dbfa4
Mahabub Alahi <mahabubalahi9531@gmail.com>
2022-08-28 21:53:18 +0800

    feat: added buyProduct smart contract function

--- chaincode/lib/estorecontract.js
+++ chaincode/lib/estorecontract.js
@@ -49,6 +49,32 @@ const Product = require('./product.js');
 
         return product;
     }
+
+    async buyProduct(ctx, vendor, name, newOwner) {
+        // Retrieve the product from the store based on its vendor and name.
+        const key = ctx.stub.createCompositeKey('PROD', [vendor, name]);
+        const productAsBytes = await ctx.stub.getState(key);
+        
+        // Check whether the corresponding document in the data store exists.
+        if (!productAsBytes || productAsBytes.length === 0) {
+            throw new Error(`${key} does not exist`);
+        }
+
+        // Deserialize the document into a product object.
+        const product = Product.deserialize(JSON.parse(productAsBytes.toString()));
+
+        // Check whether the product has already been bought.
+        if (product.getIsBought()) {
+            throw new Error(`${key} is not available for purchase`);
+        }
+
+        // Update the product in the data store.
+        product.setOwner(newOwner);
+        product.setIsBought();
+        await ctx.stub.putState(key, Buffer.from(JSON.stringify(product)));
+
+        return product;
+    }
  }
 
  module.exports = EStoreContract;
